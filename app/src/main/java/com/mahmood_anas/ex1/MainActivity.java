9f7990514cc446cb418b66f5f040302bd81a3440
mahmoodnael <m7mood.nael.1996@gmail.com>
2019-11-14 11:10:26 +0200

    by Mahmood

--- app/src/main/java/com/mahmood_anas/ex1/MainActivity.java
+++ app/src/main/java/com/mahmood_anas/ex1/MainActivity.java
@@ -1,17 +1,86 @@
 package com.mahmood_anas.ex1;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
 
+    Button StartPlay;
+    Switch Music;
+    SharedPreferences sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StartPlay = findViewById(R.id.start_play);
+
+        Music = (Switch)findViewById(R.id.music_switch);
+
+        sp = getSharedPreferences("MyPref",MODE_PRIVATE);
+        Music.setOnCheckedChangeListener(this);
+        StartPlay.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.about_menu:   // when about clicked in menu
+                openAboutDialog();
+                return true;
+            case R.id.exit_menu:    // when exit clicked in menu
+                openExitDialog();
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void openExitDialog() {
+        final AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setIcon(R.drawable.puzzle_icone);
+        alertDialog.setTitle("Exit Puzzle 15");
+        alertDialog.setMessage("Do you really want to exit the app?");
+        alertDialog.setCancelable(false);
+        alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        alertDialog.show();
+    }
+
+    private void openAboutDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setIcon(R.drawable.puzzle_icone);
+        alertDialog.setTitle("About Puzzle 15");
+        alertDialog.setMessage("This app implements the Game of Fifteen.\nBy Mahmood and Anas (c).");
+        alertDialog.setCancelable(true);
+        alertDialog.show();
+
     }
 
     @Override
@@ -20,4 +89,29 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.my_menu,menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.start_play) {           // this when the start play button clicked
+            Intent start = new Intent(this, GameActivity.class);
+            startActivity(start);
+        }
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        if(compoundButton.getId() == Music.getId() && b == true){   // this when the music switch is on
+            SharedPreferences.Editor editor =sp.edit();
+            editor.putBoolean("Music",true);
+            editor.commit();
+
+        }
+        else if(compoundButton.getId() == Music.getId() && b == true){
+            SharedPreferences.Editor editor =sp.edit();
+            editor.putBoolean("Music",false);
+            editor.commit();
+        }
+    }
+
 }

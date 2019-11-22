e7e44364ce4eb8a1cbee1be9d66a2ef73f9c5685
mahmoodnael <m7mood.nael.1996@gmail.com>
2019-11-22 20:09:44 +0200

    by Mahmood

--- app/src/main/java/com/mahmood_anas/ex1/GameActivity.java
+++ app/src/main/java/com/mahmood_anas/ex1/GameActivity.java
@@ -34,8 +34,149 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 gameBoard.shuffle();
+                if(gameBoard.isSolved() == false) {
+                    String s = "we cant solve";
+                    gameBoard.shuffle();
+                    Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+                }
+                else {
+                    String s = "we can solve";
+                    gameBoard.shuffle();
+                    Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+                }
+
             }
         });
+
+
+
+            gameBoard.array[0].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                gameBoard.swap(gameBoard.array[0]);
+
+                }
+            });
+
+            gameBoard.array[1].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                gameBoard.swap(gameBoard.array[1]);
+
+                }
+            });
+
+            gameBoard.array[2].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                gameBoard.swap(gameBoard.array[2]);
+
+                }
+            });
+
+            gameBoard.array[3].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                gameBoard.swap(gameBoard.array[3]);
+
+                }
+            });
+
+            gameBoard.array[4].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                gameBoard.swap(gameBoard.array[4]);
+
+                }
+            });
+
+            gameBoard.array[5].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                gameBoard.swap(gameBoard.array[5]);
+
+                }
+            });
+
+            gameBoard.array[6].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                gameBoard.swap(gameBoard.array[6]);
+
+                }
+            });
+
+            gameBoard.array[7].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                gameBoard.swap(gameBoard.array[7]);
+
+                }
+            });
+
+            gameBoard.array[8].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                gameBoard.swap(gameBoard.array[8]);
+
+                }
+            });
+
+            gameBoard.array[9].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                gameBoard.swap(gameBoard.array[9]);
+
+                }
+            });
+
+            gameBoard.array[10].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                gameBoard.swap(gameBoard.array[10]);
+
+                }
+            });
+
+            gameBoard.array[11].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                gameBoard.swap(gameBoard.array[11]);
+
+                }
+            });
+
+            gameBoard.array[12].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                gameBoard.swap(gameBoard.array[12]);
+
+                }
+            });
+
+            gameBoard.array[13].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                gameBoard.swap(gameBoard.array[13]);
+
+                }
+            });
+
+            gameBoard.array[14].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                gameBoard.swap(gameBoard.array[14]);
+
+                }
+            });
+
+            gameBoard.array[15].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                gameBoard.swap(gameBoard.array[15]);
+
+                }
+            });
 
 
 
@@ -82,22 +223,22 @@ public class GameActivity extends AppCompatActivity {
 
 
     public void init_ides(TextView ides[]){
-        ides[0] = findViewById(R.id.m4x4);
-        ides[1] = findViewById(R.id.m1x1);
-        ides[2] = findViewById(R.id.m1x2);
-        ides[3] = findViewById(R.id.m1x3);
-        ides[4] = findViewById(R.id.m2x4);
-        ides[5] = findViewById(R.id.m2x1);
-        ides[6] = findViewById(R.id.m2x2);
-        ides[7] = findViewById(R.id.m2x3);
-        ides[8] = findViewById(R.id.m3x4);
-        ides[9] = findViewById(R.id.m3x1);
-        ides[10] = findViewById(R.id.m3x2);
-        ides[11] = findViewById(R.id.m3x3);
-        ides[12] = findViewById(R.id.m4x4);
-        ides[13] = findViewById(R.id.m4x1);
-        ides[14] = findViewById(R.id.m4x2);
-        ides[15] = findViewById(R.id.m4x3);
+        ides[0] = findViewById(R.id.m1x1);
+        ides[1] = findViewById(R.id.m1x2);
+        ides[2] = findViewById(R.id.m1x3);
+        ides[3] = findViewById(R.id.m1x4);
+        ides[4] = findViewById(R.id.m2x1);
+        ides[5] = findViewById(R.id.m2x2);
+        ides[6] = findViewById(R.id.m2x3);
+        ides[7] = findViewById(R.id.m2x4);
+        ides[8] = findViewById(R.id.m3x1);
+        ides[9] = findViewById(R.id.m3x2);
+        ides[10] = findViewById(R.id.m3x3);
+        ides[11] = findViewById(R.id.m3x4);
+        ides[12] = findViewById(R.id.m4x1);
+        ides[13] = findViewById(R.id.m4x2);
+        ides[14] = findViewById(R.id.m4x3);
+        ides[15] = findViewById(R.id.m4x4);
 
     }
 
--- app/src/main/java/com/mahmood_anas/ex1/GameBoard.java
+++ app/src/main/java/com/mahmood_anas/ex1/GameBoard.java
@@ -35,16 +35,137 @@ public class GameBoard {
             array[i].setBackground(d);
         }
 
-
    }
+   public void swap(TextView tv){
+        if(tv.getText().toString().isEmpty()== true)
+            return;
 
-   public void swap(){
-        this.array[1].setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
+        int pos =0;
+        int emptyPos = 0;
+        for(int i=0;i<16;i++){
+            if(tv.getId() == this.array[i].getId())
+                pos = i;
+            if(this.array[i].getText().toString().isEmpty())
+                emptyPos=i;
+        }
+       Log.d("pos\tEmptyPos",pos + "\t" + emptyPos);
+
+
+        if(pos < 4){
+            if(pos%4 == 0 &&(pos == emptyPos-1 || pos == emptyPos-4)) { // swap
+                swap_ele(this.array[pos], this.array[emptyPos]);
+                Log.d("pos\tEmptyPos",pos + "\t" + emptyPos);
             }
-        });
+
+            else if(pos%4 ==3 &&(pos == emptyPos+1 || pos == emptyPos-4)) {//swap
+                swap_ele(this.array[pos], this.array[emptyPos]);
+                Log.d("pos\tEmptyPos",pos + "\t" + emptyPos);
+            }
+
+            else if(pos == emptyPos+1 || pos == emptyPos- 1 || pos == emptyPos-4) {//swap
+                swap_ele(this.array[pos], this.array[emptyPos]);
+                Log.d("pos\tEmptyPos",pos + "\t" + emptyPos);
+            }
+
+
+        }
+        else if(pos > 11){
+            if(pos%4 == 0 && (pos == emptyPos -1 || pos == emptyPos +4)) {
+                swap_ele(this.array[pos], this.array[emptyPos]);
+                Log.d("pos\tEmptyPos", pos + "\t" + emptyPos);
+            }
+            else if(pos%4 == 3 && (pos == emptyPos+1 || pos == emptyPos+4)) {
+                swap_ele(this.array[pos], this.array[emptyPos]);
+                Log.d("pos\tEmptyPos", pos + "\t" + emptyPos);
+            }
+            else if(pos == emptyPos+1 || pos == emptyPos- 1 || pos == emptyPos+4) { //swap
+                swap_ele(this.array[pos], this.array[emptyPos]);
+                Log.d("pos\tEmptyPos", pos + "\t" + emptyPos);
+            }
+        }
+
+        else{
+            if(pos%4 == 0 &&(pos == emptyPos-1 || pos == emptyPos+4 || pos == emptyPos-4 )){
+                swap_ele(this.array[pos],this.array[emptyPos]);
+                Log.d("pos\tEmptyPos",pos + "\t" + emptyPos);
+            }
+            else if(pos%4 == 3 &&(pos == emptyPos+1 || pos == emptyPos+4 || pos == emptyPos-4 )){
+                swap_ele(this.array[pos],this.array[emptyPos]);
+                Log.d("pos\tEmptyPos",pos + "\t" + emptyPos);
+            }
+            else if((pos%4 == 2 || pos%4==1 ) &&(pos == emptyPos+1 || pos == emptyPos+1 || pos == emptyPos+4 || pos == emptyPos-4 )){
+                swap_ele(this.array[pos],this.array[emptyPos]);
+                Log.d("pos\tEmptyPos",pos + "\t" + emptyPos);
+            }
+
+
+        }
+
    }
 
+   public void swap_ele(TextView first,TextView sec){
+
+       String ss = first.getText().toString();
+       Drawable d =first.getBackground();
+
+
+       first.setText( sec.getText().toString());
+       first.setBackground(sec.getBackground());
+
+       sec.setText(ss);
+       sec.setBackground(d);
+   }
+    public boolean isSolved(){
+        int parity =0;
+        int gridWidth = (int) Math.sqrt(this.array.length);
+        int row =0;
+        int blankrow = 0;
+        int i =0;
+        while (i < 16){
+            String for_i =this.array[i].getText().toString();
+            int for_i_int;
+            if(for_i.isEmpty() == true)
+                for_i_int = 0;
+            else
+                for_i_int = Integer.parseInt(for_i);
+            if (i % gridWidth == 0){
+                row++;
+            }
+            if (for_i_int == 0){
+                blankrow = row;
+                break;
+            }
+            for (int j = i + 1;j < this.array.length;j++){
+
+
+                    int for_j_int;
+                    String for_j =this.array[j].getText().toString();
+                    if(for_j.isEmpty() == true)
+                        for_j_int = 0;
+                    else
+                        for_j_int = Integer.parseInt(for_i);
+
+
+
+                if (for_i_int > for_j_int && for_i_int != 0){
+
+                    parity++;
+                }
+            }
+            i++;
+        }
+        if(gridWidth%2 == 0){
+            if(blankrow%2 == 0){
+                return parity%2 == 0;
+            }
+            else{
+                return parity%2 != 0;
+            }
+        }
+        else{
+            return parity %2 ==0;
+        }
+
+    }
 }
